3bb72451c6af7f1c82ce5d1b2df7cf29dd6d5022
vbegunova <veronikabegunova42@gmail.com>
2023-07-24 11:14:50 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,12 +27,13 @@ export class App extends Component {
 
   render() {
     const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
 
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
+            options={options}
             onLeaveFeedback={this.addFeedback}
           />
         </Section>
